609a3640d9b6100f935000354ce408234fdfe675
lymbycfyk <lymbycfyk@posteo.de>
2017-11-12 20:50:04 +0100

    a bit organizing

--- setup.py
+++ setup.py
@@ -7,14 +7,17 @@ def readme():
 
 
 setup(name='noisepy',
-      version='0.1.4.1',
+      version='0.1.4.2',
       description='generates random noisey b/w images',
       long_description=readme(),
       classifiers=[
+          'Environment :: Console',
           'Development Status :: 3 - Alpha',
           'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.6',
           'Topic :: Multimedia :: Graphics',
+          'Topic :: Artistic Software',
       ],
       keywords='noisepy noise image generator snow random',
       url='https://lymbycfyk.github.io/noisepy/',
@@ -22,6 +25,7 @@ setup(name='noisepy',
       author_email='lymbycfyk@posteo.de',
       license='MIT',
       packages=['noisepy'],
+      python_requires='>=3',
       install_requires=[
           'numpy',
           'scipy',
